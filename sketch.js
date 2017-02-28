fc4e1f959da2b4cc94127020266df53ecd3813ca
Savion Triplett <striplett@wlapcs.org>
2017-02-28 20:41:22 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup(){
+    createCanvas(600,400);
+    background(100,100,100);
+}
+
+function draw(){
+    line(35,80,400,200);
+    
+    stroke(0,255,255);
+    fill(255,0,0);
+    rect(290,100,250,250);
+    
+    stroke(20,20,20);
+    strokeWeight(20);
+    fill(0,0,255);
+    ellipse(50,240,80,80);
+}   
